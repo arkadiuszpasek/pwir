621fbef4863d7c89ef393a2ba751093bc82f01a5
Arkadiusz Pasek <thevik444@gmail.com>
2021-01-16 18:15:37 +0100

    remove unused

--- protocol/protocol.erl
+++
@@ -1,9 +0,0 @@
--module(protocol).
-
--export([create_data_payload/1, create_action_payload/2]).
-
-create_data_payload(Data) -> 
-  {data, Data}.
-
-create_action_payload(Type, Action) ->
-  {action, Type, Action}.
